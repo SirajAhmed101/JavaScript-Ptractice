de2481aeec7e21639466f117e01a4e6c802b2c38
Siraj Ahmed <mdsiraj101@gmail.com>
2023-04-14 11:23:08 +0530

    done

--- Loops_with_arrays.js
+++ Loops_with_arrays.js
@@ -1,5 +1,5 @@
 // for loop
-// console.log(for loop with array);
+console.log("for loop with array");
 
 let arr = [12, 65, 86, 62, 12, 48, 30, 25];
 
@@ -8,14 +8,14 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 // forEach Loop
-// console.log(forEach Loop with array);
+console.log("forEach Loop with array");
 
 arr.forEach((Element) => {
   console.log(Element + Element);
 });
 
 // Array.from
-// console.log(Array from);
+console.log("Array from");
 
 let name = "siraj";
 let arrFrom = Array.from(name);
@@ -23,14 +23,14 @@ let arrFrom = Array.from(name);
 console.log(arrFrom);
 
 //for of loop
-// console.log(for of loop);
+console.log("for of loop");
 
 for (num of arr) {
   console.log(num);
 }
 
 //for in loop
-// console.log(for in loop);
+console.log("for in loop");
 
 for (i in arr) {
   console.log(i);
